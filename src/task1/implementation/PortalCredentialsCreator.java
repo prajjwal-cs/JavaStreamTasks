c02e43c6d0e24efa8776fcee6b7e04063e94f3b6
Prajjwal <prajjwal199275@gmail.com>
2022-03-18 22:59:00 +0530

    Implemented Documentation for each method

--- src/task1/implementation/PortalCredentialsCreator.java
+++ src/task1/implementation/PortalCredentialsCreator.java
@@ -14,6 +14,12 @@ import java.util.List;
 
 public class PortalCredentialsCreator {
 
+    /**
+     * Create a username from the student's first name, last name, roll number and current year
+     *
+     * @param student The student object that we want to create a username for.
+     * @return Nothing.
+     */
     private UserName createUsername(Student student) {
         var firstName = student.getFirstName();
         var lastName = student.getLastName();
@@ -25,14 +31,32 @@ public class PortalCredentialsCreator {
         return new UserName(userName);
     }
 
+    /**
+     * Create a list of user names from a list of students
+     *
+     * @param studentsList the list of students to create usernames for
+     * @return A list of UserName objects.
+     */
     public List<UserName> createListOfUserNames(List<Student> studentsList) {
         return studentsList.stream().map(this::createUsername).toList();
     }
 
+    /**
+     * Create a random alphanumeric string of length 14
+     *
+     * @param student The student object that is being created.
+     * @return A String of random alphanumeric characters.
+     */
     private String createPassword(Student student) {
         return RandomStringUtils.randomAlphanumeric(14);
     }
 
+    /**
+     * Create a list of passwords for each student in the list of students
+     *
+     * @param studentsList a list of students
+     * @return A list of PassWord objects.
+     */
     public List<PassWord> createListOfRandomPasswords(List<Student> studentsList) {
         return studentsList.stream().map(this::createPassword).map(PassWord::new).toList();
     }
