d910045ef2035840b8961782d7fb882496f1d82b
Prajjwal <prajjwal199275@gmail.com>
2022-03-18 22:57:19 +0530

    Implemented Documentation

--- src/task2/implementation/TestResultGenerator.java
+++ src/task2/implementation/TestResultGenerator.java
@@ -13,15 +13,33 @@ import java.util.stream.Collectors;
 
 public class TestResultGenerator {
 
+    /**
+     * It concatenates the student name and the guardians contact number.
+     *
+     * @param student The student object that is being passed to the method.
+     * @return The concatenated string of the student name and the guardians contact number.
+     */
     private String studentDetails(Student student) {
         return student.getStudentName().concat("-").concat(String.valueOf(student.getGuardiansContactNumber()));
     }
 
+    /**
+     * Given a set of students, return a set of strings containing the details of the students who failed
+     *
+     * @param studentSet The set of students for which we want to get the details of the students who failed.
+     * @return A HashSet of Strings.
+     */
     public HashSet<String> getDetailsOfFailedStudents(Set<Student> studentSet) {
         return studentSet.stream().filter(student -> student.getMarks() < 33.0)
                 .map(this::studentDetails).collect(Collectors.toCollection(HashSet<String>::new));
     }
 
+    /**
+     * Given a set of students, return a set of students sorted by their marks
+     *
+     * @param studentSet The set of students to be sorted.
+     * @return A set of students sorted by their marks.
+     */
     public Set<Student> sortStudentsForRanking(HashSet<Student> studentSet) {
         Comparator<Student> markSorting = (s1, s2) -> Double.compare(s1.getMarks(), s2.getMarks());
 
